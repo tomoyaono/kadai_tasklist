311d7a882a282dc976a095ee3e2d87db716579fa
tomoya.ono <oh_tomoya_gogo@icloud.com>
2018-05-01 11:40:37 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+
+    if @tasks.save
+      flash[:success] = 'タスクが正常に反映されました'
+      redirect_to @tasks
+    else
+      flash.now[:danger] = 'タスクが反映されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+
+    if @tasks.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @tasks
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end  
+end
